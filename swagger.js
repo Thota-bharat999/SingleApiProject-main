7a4021f9392a235b21b2e3d120a0dddfae57f8f5
Thota-bharat999 <bharatmanithota@gmail.com>
2025-08-26 18:01:48 +0530

    ..

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'https://singleapiproject-main-12.onrender.com', // Local root
+        url: 'https://singleapiproject-main-13.onrender.com', // Local root
         description: 'Local API Server',
       },
     ],
